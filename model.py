2f7ef86bf55c00ce2d3c8cd9292ccc82c19dfeb4
Patrick <pbryant@ae18a.scilifelab.se>
2020-09-08 12:33:00 +0200

    making data

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ import glob
 import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
-from scipy.stats import gamma
+from scipy.stats import gamma, lognorm
 import numpy as np
 import seaborn as sns
 import networkx as nx
@@ -36,6 +36,18 @@ def conv_gamma_params(mean,std):
 
         return shape,scale
 
+def conv_lognorm_params(mean,std):
+        '''Returns converted shape and scale params
+        A common parametrization for a lognormal random variable Y is in terms of the mean, mu, and standard deviation, sigma,
+        of the unique normally distributed random variable X such that exp(X) = Y.
+        This parametrization corresponds to setting s = sigma and scale = exp(mu).
+        lognorm.pdf(x, s, loc, scale)
+        '''
+        shape = std
+        scale = np.exp(mean)
+
+        return shape,scale
+
 def infection_to_death():
         '''Simulate the time from infection to death: Infection --> Onset --> Death'''
         #Infection to death: sum of ito and otd
@@ -43,20 +55,21 @@ def infection_to_death():
         itd = gamma(a=itd_shape, scale = itd_scale) #a=shape
         return itd
 
-def serial_interval_distribution(N2):
+def serial_interval_distribution(N):
         '''Models the the time between when a person gets infected and when
         they subsequently infect another other people
         '''
-        serial_shape, serial_scale = conv_gamma_params(4.7,2.9) #https://www.sciencedirect.com/science/article/pii/S1201971220301193
-        serial = gamma(a=serial_shape, scale = serial_scale) #a=shape
+        serial_shape, serial_scale = conv_lognorm_params(4.7,2.9) #https://www.sciencedirect.com/science/article/pii/S1201971220301193
+        serial = lognorm(s=serial_shape, scale = serial_scale) #a=shape
 
-        return serial.pdf(np.arange(1,N2+1))
+        return serial.pdf(np.arange(1,N+1))
 
 ###Data formatting
-def read_and_format_data(datadir):
+def read_and_format_data(datadir, outdir):
         '''Read in and format all data needed for the model
-        N2 = number of days to model
+        N = number of days to model
         '''
+        N=300
 
         #Get epidemic data
         epidemic_data = pd.read_csv(datadir+'stockholm.csv')
@@ -67,12 +80,13 @@ def read_and_format_data(datadir):
         #mobility_data['date']=pd.to_datetime(mobility_data['date'], format='%Y/%m/%d')
 
         #SI
-        serial_interval = serial_interval_distribution(N2) #pd.read_csv(datadir+"serial_interval.csv")
-
+        serial_interval = serial_interval_distribution(N) #pd.read_csv(datadir+"serial_interval.csv")
+        #Infection fatality rate
+        ifr=0.0058
         #Infection to death distribution
         itd = infection_to_death()
         #Get hazard rates for all days in country data
-        h = np.zeros(N2) #N2 = N+forecast
+        h = np.zeros(N)
         f = np.cumsum(itd.pdf(np.arange(1,len(h)+1,0.5))) #Cumulative probability to die for each day
         #Adjust f to reach max 1 - the half steps makes this different
         f = f/2
@@ -80,11 +94,11 @@ def read_and_format_data(datadir):
             #for each day t, the death prob is the area btw [t-0.5, t+0.5]
             #divided by the survival fraction (1-the previous death fraction), (fatality ratio*death prob at t-0.5)
             #This will be the percent increase compared to the previous end interval
-            h[i] = (cfr*(f[i*2+1]-f[i*2-1]))/(1-cfr*f[i*2-1])
+            h[i] = (ifr*(f[i*2+1]-f[i*2-1]))/(1-ifr*f[i*2-1])
 
         #The number of deaths today is the sum of the past infections weighted by their probability of death,
         #where the probability of death depends on the number of days since infection.
-        s = np.zeros(N2)
+        s = np.zeros(N)
         s[0] = 1
         for i in range(1,len(s)):
             #h is the percent increase in death
@@ -98,10 +112,16 @@ def read_and_format_data(datadir):
         #Multiplying s and h yields fraction dead of fraction survived
         f = s*h #This will be fed to the Stan Model
         plt.plot(np.arange(len(f)),f)
-        plt.show()
+        plt.savefig(outdir+'f.png')
+        plt.close()
+        plt.plot(np.arange(len(serial_interval)),serial_interval)
+        plt.savefig(outdir+'SI.png')
         pdb.set_trace()
 
         #Network
+        numnodes = 2385643
+        m = 5 #Number of edges to attach from a new node to existing nodes
+        G_barabasi = nx.barabasi_albert_graph(n,m)
 
 
 def simulate():
@@ -117,6 +137,6 @@ datadir = args.datadir[0]
 outdir = args.outdir[0]
 
 #Read and format data
-read_and_format_data(datadir)
+read_and_format_data(datadir, outdir)
 #Simulate
 out = simulate(stan_data, stan_model, outdir)
