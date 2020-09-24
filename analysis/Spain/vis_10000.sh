0dee42ecc73b0f55cb7930cba26aff8d8503c086
Patrick <pbryant@ae18a.scilifelab.se>
2020-09-24 13:44:34 +0200

    changed name

--- analysis/Spain/vis_10000.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-DATADIR=../../data/
-RESULTSDIR=../../results/Spain/n10000/
-N=10000
-OUTDIR=../../results/Spain/n10000/plots/
-
-./vis.py --datadir $DATADIR --resultsdir $RESULTSDIR --n $N --outdir $OUTDIR
